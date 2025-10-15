bfaa1540573b4fb752dbaeb93070f40be7dfa7b0
Aly <d-aly@outlook.fr>
2025-10-15 23:01:35 +0200

    Update Version20251015201543.php

--- migrations/Version20251015201543.php
+++ migrations/Version20251015201543.php
@@ -7,29 +7,22 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20251015201543 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Suppression du champ erroné owner_id dans User';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE symfony_demo_user ADD owner_id INT NOT NULL');
-        $this->addSql('ALTER TABLE symfony_demo_user ADD CONSTRAINT FK_8FB094A17E3C61F9 FOREIGN KEY (owner_id) REFERENCES symfony_demo_user (id)');
-        $this->addSql('CREATE INDEX IDX_8FB094A17E3C61F9 ON symfony_demo_user (owner_id)');
+        // Le champ owner_id n’a plus lieu d’être dans symfony_demo_user
+        // On ne fait rien ici car la correction est gérée dans une autre migration
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE symfony_demo_user DROP FOREIGN KEY FK_8FB094A17E3C61F9');
-        $this->addSql('DROP INDEX IDX_8FB094A17E3C61F9 ON symfony_demo_user');
-        $this->addSql('ALTER TABLE symfony_demo_user DROP owner_id');
+        // Si rollback, on peut réintroduire le champ erroné (optionnel)
+        $this->addSql('ALTER TABLE symfony_demo_user ADD owner_id INT DEFAULT NULL');
     }
 }
