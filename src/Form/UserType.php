2e490988de4c2eeeff916d3362d7b1734cf3cb1c
Aly <d-aly@outlook.fr>
2025-10-15 22:39:27 +0200

    Update UserType.php

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -1,53 +1,44 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-/**
- * Defines the form used to edit an user.
- *
- * @author Romain Monteil <monteil.romain@gmail.com>
- */
-final class UserType extends AbstractType
+class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        // For the full reference of options defined by each form field type
-        // see https://symfony.com/doc/current/reference/forms/types.html
-
-        // By default, form fields include the 'required' attribute, which enables
-        // the client-side form validation. This means that you can't test the
-        // server-side validation errors from the browser. To temporarily disable
-        // this validation, set the 'required' attribute to 'false':
-        // $builder->add('title', null, ['required' => false, ...]);
-
         $builder
-            ->add('username', TextType::class, [
-                'label' => 'label.username',
-                'disabled' => true,
-            ])
             ->add('fullName', TextType::class, [
-                'label' => 'label.fullname',
+                'label' => 'Nom complet',
+                'attr' => ['class' => 'form-control rounded-pill'],
             ])
             ->add('email', EmailType::class, [
-                'label' => 'label.email',
+                'label' => 'Adresse email',
+                'attr' => ['class' => 'form-control rounded-pill'],
             ])
-        ;
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'Téléphone',
+                'required' => false,
+                'attr' => ['class' => 'form-control rounded-pill'],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message personnel',
+                'required' => false,
+                'attr' => ['class' => 'form-control rounded-pill', 'rows' => 4],
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'required' => false,
+                'attr' => ['class' => 'form-control rounded-pill'],
+            ]);
+            // Tu peux ajouter 'avatar' ici si tu veux gérer l'upload dans ce formulaire
     }
 
     public function configureOptions(OptionsResolver $resolver): void
