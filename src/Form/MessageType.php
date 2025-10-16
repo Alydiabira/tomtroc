859b296a2c40da7f59ceaac4cdf8de3c343453ba
Aly <d-aly@outlook.fr>
2025-10-16 13:02:43 +0200

    Create MessageType.php

---
+++ src/Form/MessageType.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form;
+
+use App\Entity\Message;
+use App\Entity\user;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content')
+            ->add('createdAt')
+            ->add('sender', EntityType::class, [
+                'class' => user::class,
+                'choice_label' => 'id',
+            ])
+            ->add('recipient', EntityType::class, [
+                'class' => user::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
