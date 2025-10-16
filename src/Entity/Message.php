b8afa4d84d929e290d88c3af435df65023d9f18a
Aly <d-aly@outlook.fr>
2025-10-16 13:02:42 +0200

    Update Message.php

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -12,6 +12,7 @@
 namespace App\Entity;
 
 use App\Repository\MessageRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
@@ -22,8 +23,68 @@ class Message
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTime $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    private ?user $sender = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    private ?user $recipient = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getSender(): ?user
+    {
+        return $this->sender;
+    }
+
+    public function setSender(?user $sender): static
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getRecipient(): ?user
+    {
+        return $this->recipient;
+    }
+
+    public function setRecipient(?user $recipient): static
+    {
+        $this->recipient = $recipient;
+
+        return $this;
     }
 }
