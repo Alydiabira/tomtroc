d4e4e648f31a72b2ba812cedc84108fa9635fc40
Aly <d-aly@outlook.fr>
2025-10-16 18:39:44 +0200

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -102,7 +102,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getUserIdentifier(): string
     {
-        return (string) $this->username;
+        return (string) $this->email;
     }
 
     public function getUsername(): string
