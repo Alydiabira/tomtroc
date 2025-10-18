6d29e2aebf7102ba613d2ef8695547f0dd9ee3bf
Aly <d-aly@outlook.fr>
2025-10-18 15:17:09 +0200

    Update MessageFixtures.php

--- src/DataFixtures/MessageFixtures.php
+++ src/DataFixtures/MessageFixtures.php
@@ -2,60 +2,70 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\User;
 use App\Entity\Conversation;
 use App\Entity\Message;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
-class MessageFixtures extends Fixture
+class MessageFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function __construct(private UserPasswordHasherInterface $passwordHasher) {}
-
     public function load(ObjectManager $manager): void
     {
-        // Création de deux utilisateurs
-        $user1 = new User();
-        $user1->setFullName('Aly Diabira');
-        $user1->setUsername('aly');
-        $user1->setEmail('aly@example.com');
-        $user1->setRoles(['ROLE_USER']);
-        $user1->setPassword($this->passwordHasher->hashPassword($user1, 'motdepasse123'));
-        $manager->persist($user1);
+        $faker = Factory::create('fr_FR');
 
-        $user2 = new User();
-        $user2->setFullName('Léna Dubois');
-        $user2->setUsername('lena');
-        $user2->setEmail('lena@example.com');
-        $user2->setRoles(['ROLE_USER']);
-        $user2->setPassword($this->passwordHasher->hashPassword($user2, 'motdepasse456'));
-        $manager->persist($user2);
+        $users = [
+            'aly' => $this->getReference('aly'),
+            'tom' => $this->getReference('tom'),
+            'nathalie' => $this->getReference('nathalie'),
+            'sasa94' => $this->getReference('sasa94'),
+        ];
 
-        // Création d'une conversation
-        $conversation = new Conversation();
-        $conversation->setUser1($user1);
-        $conversation->setUser2($user2);
-        $conversation->setLastMessageAt(new \DateTimeImmutable('-1 hour'));
-        $manager->persist($conversation);
+        $pairs = [
+            ['aly', 'tom'],
+            ['aly', 'nathalie'],
+            ['aly', 'sasa94'],
+            ['tom', 'nathalie'],
+        ];
 
-        // Création de messages
-        $message1 = new Message();
-        $message1->setSender($user1);
-        $message1->setRecipient($user2);
-        $message1->setConversation($conversation);
-        $message1->setContent("Salut Léna, tu es dispo pour échanger ?");
-        $message1->setCreatedAt(new \DateTimeImmutable('-50 minutes'));
-        $manager->persist($message1);
+        foreach ($pairs as [$userA, $userB]) {
+            $conversation = new Conversation();
+            $conversation->setUser1($users[$userA]);
+            $conversation->setUser2($users[$userB]);
+            $conversation->setLastMessageAt(new \DateTimeImmutable('-' . random_int(1, 5) . ' hours'));
 
-        $message2 = new Message();
-        $message2->setSender($user2);
-        $message2->setRecipient($user1);
-        $message2->setConversation($conversation);
-        $message2->setContent("Coucou Aly ! Oui, je suis dispo maintenant 😊");
-        $message2->setCreatedAt(new \DateTimeImmutable('-45 minutes'));
-        $manager->persist($message2);
+            $manager->persist($conversation);
+
+            for ($i = 0; $i < 5; $i++) {
+                $sender = $i % 2 === 0 ? $users[$userA] : $users[$userB];
+                $recipient = $i % 2 === 0 ? $users[$userB] : $users[$userA];
+
+                $message = new Message();
+                $message->setSender($sender);
+                $message->setRecipient($recipient);
+                $message->setConversation($conversation);
+                $message->setContent($faker->randomElement([
+                    "Tu veux troquer ce livre 📚 ?",
+                    "J’ai trouvé un super vinyle 🎶, ça t’intéresse ?",
+                    "Dispo pour un échange demain ? 😊",
+                    "Je peux passer à Rosny vers 18h 🕕",
+                    "Top ton annonce ! Tu cherches quoi en échange ? 🔄",
+                    $faker->realTextBetween(40, 120),
+                ]));
+                $message->setCreatedAt((new \DateTimeImmutable())->modify('-' . ($i * 10) . ' minutes'));
+
+                $manager->persist($message);
+            }
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+        ];
     }
 }
