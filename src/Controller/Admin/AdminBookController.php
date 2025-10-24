b800dd7f3808366a7a1bb49bc13578f2bf7b1e6b
Aly <d-aly@outlook.fr>
2025-10-24 16:05:37 +0200

    Update AdminBookController.php

--- src/Controller/Admin/AdminBookController.php
+++ src/Controller/Admin/AdminBookController.php
@@ -9,19 +9,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/book')]
 class AdminBookController extends AbstractController
 {
     #[Route('/{id}/edit', name: 'admin_book_edit')]
-    public function edit(Book $book, Request $request, EntityManagerInterface $em): Response
+    public function edit(Book $book, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(BookType::class, $book);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $coverFile = $form->get('coverImage')->getData();
+
+            if ($coverFile) {
+                $originalFilename = pathinfo($coverFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $coverFile->guessExtension();
+
+                try {
+                    $coverFile->move(
+                        $this->getParameter('covers_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('danger', 'Erreur lors de l’upload de l’image.');
+                }
+
+                $book->setCoverImage($newFilename);
+            }
+
             $em->flush();
-            return $this->redirectToRoute('user_books');
+            $this->addFlash('success', '📘 Livre mis à jour avec succès !');
+
+            return $this->redirectToRoute('admin_book_index');
         }
 
         return $this->render('admin/book/edit.html.twig', [
@@ -31,11 +54,14 @@ class AdminBookController extends AbstractController
     }
 
     #[Route('/{id}/delete', name: 'admin_book_delete', methods: ['POST'])]
-    public function delete(Book $book, EntityManagerInterface $em): Response
+    public function delete(Book $book, Request $request, EntityManagerInterface $em): Response
     {
-        $em->remove($book);
-        $em->flush();
+        if ($this->isCsrfTokenValid('delete' . $book->getId(), $request->request->get('_token'))) {
+            $em->remove($book);
+            $em->flush();
+            $this->addFlash('danger', '📕 Livre supprimé.');
+        }
 
-        return $this->redirectToRoute('user_books');
+        return $this->redirectToRoute('admin_book_index');
     }
 }
