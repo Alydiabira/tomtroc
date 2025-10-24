877f4219fae5d804e5eab6d69189f28239fb5207
Aly <d-aly@outlook.fr>
2025-10-24 21:08:58 +0200

    Update PublicProfileController.php

--- src/Controller/PublicProfileController.php
+++ src/Controller/PublicProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,19 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/profil')]
 class PublicProfileController extends AbstractController
 {
-  #[Route('/profil/{slug}', name: 'public_profile')]
-public function show(UserRepository $userRepository, string $slug): Response
-{
-    $user = $userRepository->findOneBy(['slug' => $slug]);
+    #[Route('/{slug}', name: 'public_profile')]
+    public function show(UserRepository $userRepository, string $slug): Response
+    {
+        $user = $userRepository->findOneBy(['slug' => $slug]);
 
-    if (!$user) {
-        throw $this->createNotFoundException('Utilisateur introuvable');
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur introuvable');
+        }
+
+        return $this->render('user/public_profile.html.twig', [
+            'user' => $user,
+        ]);
     }
-
-    return $this->render('user/public_profile.html.twig', [
-        'user' => $user,
-    ]);
-}
-
-
 }
