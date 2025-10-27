1c9feb3004b60a17e08669a2882f47726c56d9cd
Aly <d-aly@outlook.fr>
2025-10-27 18:41:37 +0100

    Update importmap.php

--- importmap.php
+++ importmap.php
@@ -32,9 +32,6 @@ return [
     '@popperjs/core' => [
         'version' => '2.11.8',
     ],
-    'bootstrap' => [
-        'version' => '5.3.8',
-    ],
     'jquery' => [
         'version' => '3.7.1',
     ],
@@ -75,16 +72,16 @@ return [
         'version' => '4.1.1',
     ],
     'es6-promise' => [
-        'version' => '3.3.1',
+        'version' => '4.2.8',
     ],
     'storage2' => [
         'version' => '0.1.2',
     ],
     'superagent' => [
-        'version' => '3.8.3',
+        'version' => '10.2.3',
     ],
     'component-emitter' => [
-        'version' => '1.3.1',
+        'version' => '2.0.0',
     ],
     'bootstrap-tagsinput' => [
         'version' => '0.7.1',
@@ -95,5 +92,98 @@ return [
     ],
     'es-module-shims' => [
         'version' => '2.6.2',
+    ],
+    'fast-safe-stringify' => [
+        'version' => '2.1.1',
+    ],
+    'qs' => [
+        'version' => '6.14.0',
+    ],
+    'side-channel' => [
+        'version' => '1.1.0',
+    ],
+    'es-errors/type' => [
+        'version' => '1.3.0',
+    ],
+    'object-inspect' => [
+        'version' => '1.13.3',
+    ],
+    'side-channel-list' => [
+        'version' => '1.0.0',
+    ],
+    'side-channel-map' => [
+        'version' => '1.0.1',
+    ],
+    'side-channel-weakmap' => [
+        'version' => '1.0.2',
+    ],
+    'get-intrinsic' => [
+        'version' => '1.2.5',
+    ],
+    'call-bound' => [
+        'version' => '1.0.2',
+    ],
+    'es-errors' => [
+        'version' => '1.3.0',
+    ],
+    'es-errors/eval' => [
+        'version' => '1.3.0',
+    ],
+    'es-errors/range' => [
+        'version' => '1.3.0',
+    ],
+    'es-errors/ref' => [
+        'version' => '1.3.0',
+    ],
+    'es-errors/syntax' => [
+        'version' => '1.3.0',
+    ],
+    'es-errors/uri' => [
+        'version' => '1.3.0',
+    ],
+    'gopd' => [
+        'version' => '1.2.0',
+    ],
+    'es-define-property' => [
+        'version' => '1.0.1',
+    ],
+    'has-symbols' => [
+        'version' => '1.1.0',
+    ],
+    'dunder-proto/get' => [
+        'version' => '1.0.0',
+    ],
+    'call-bind-apply-helpers/functionApply' => [
+        'version' => '1.0.0',
+    ],
+    'call-bind-apply-helpers/functionCall' => [
+        'version' => '1.0.0',
+    ],
+    'function-bind' => [
+        'version' => '1.1.2',
+    ],
+    'hasown' => [
+        'version' => '2.0.2',
+    ],
+    'call-bind' => [
+        'version' => '1.0.8',
+    ],
+    'call-bind-apply-helpers' => [
+        'version' => '1.0.0',
+    ],
+    'set-function-length' => [
+        'version' => '1.2.2',
+    ],
+    'call-bind-apply-helpers/applyBind' => [
+        'version' => '1.0.0',
+    ],
+    'define-data-property' => [
+        'version' => '1.1.4',
+    ],
+    'has-property-descriptors' => [
+        'version' => '1.0.2',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.8',
     ],
 ];
