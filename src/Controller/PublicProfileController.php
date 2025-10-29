661407ae356077e7a974f1c7c8fcdbbc7539dcd8
Aly <d-aly@outlook.fr>
2025-10-30 00:45:12 +0100

    Update PublicProfileController.php

--- src/Controller/PublicProfileController.php
+++ src/Controller/PublicProfileController.php
@@ -7,21 +7,38 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 
 #[Route('/profil')]
 class PublicProfileController extends AbstractController
 {
-    #[Route('/{slug}', name: 'public_profile')]
-    public function show(UserRepository $userRepository, string $slug): Response
-    {
-        $user = $userRepository->findOneBy(['slug' => $slug]);
+#[Route('/{slug}', name: 'public_profile')]
+public function show(UserRepository $userRepository, string $slug): Response
+{
+    $user = $userRepository->findOneBy(['slug' => $slug]);
 
-        if (!$user) {
-            throw $this->createNotFoundException('Utilisateur introuvable');
-        }
-
-        return $this->render('user/public_profile.html.twig', [
-            'user' => $user,
-        ]);
+    if (!$user) {
+        throw $this->createNotFoundException('Utilisateur introuvable');
     }
+
+    // 🔒 Si l'utilisateur connecté accède à son propre profil, rediriger vers "Mon compte"
+    if ($this->getUser() && $this->getUser()->getId() === $user->getId()) {
+        return $this->redirectToRoute('user_account');
+    }
+
+    $otherUsers = $userRepository->createQueryBuilder('u')
+        ->where('u.slug != :slug')
+        ->setParameter('slug', $slug)
+        ->setMaxResults(5)
+        ->orderBy('u.createdAt', 'DESC')
+        ->getQuery()
+        ->getResult();
+
+    return $this->render('user/public_profile.html.twig', [
+        'user' => $user,
+        'otherUsers' => $otherUsers,
+    ]);
+}
+
 }
